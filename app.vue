8482ef1d86c417ee35e84c0f5428abdaf86bfa19
Klein Petr <petr@kleinpetr.com>
2024-11-19 17:52:28 +0100

    feat: minimal bug repro

---
+++ app.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import { SolanaAdapter } from '@reown/appkit-adapter-solana'
+import { SolflareWalletAdapter, PhantomWalletAdapter } from '@solana/wallet-adapter-wallets'
+
+const solanaWeb3JsAdapter = new SolanaAdapter({
+  wallets: [new PhantomWalletAdapter(), new SolflareWalletAdapter()],
+})
+</script>
+
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
