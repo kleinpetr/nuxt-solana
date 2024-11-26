9419992eadaea831c6b802cd54d72f04c332f802
Daniel Roe <daniel@roe.dev>
2024-11-26 10:34:18 +0000

    fix: add aliases

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  alias: {
+    'buffer/': 'buffer/',
+    'string_decoder/': 'string_decoder/',
+    'process/': 'process/',
+    'pino-pretty': 'unenv/runtime/mock/proxy'
+  },
   nitro: {
     preset: 'cloudflare-pages'
   }
